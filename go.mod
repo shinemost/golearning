d1497bddb0cbe82418d84acaefd21ac65595b577
shinemost <supertain147@163.com>
2023-12-24 22:14:30 +0800

    使用另一个三方包alphadose/hasxmap作为无锁线程安全的map，与hashmap库相同的是在写入时也需要等待扩容完成，默认队列长度为50，每次扩容增加一倍，不过可以在初始化map时指定队列长度，这样可以减少扩容操作。 https://github.com/alphadose/haxmap

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module golearning
 go 1.21
 
 require (
-	github.com/cornelk/hashmap v1.0.8
+	github.com/alphadose/haxmap v1.3.1
 	github.com/orcaman/concurrent-map v1.0.0
 )
+
+require golang.org/x/exp v0.0.0-20221031165847-c99f073a8326 // indirect
--- main.go
+++ main.go
@@ -2,21 +2,41 @@ package main
 
 import (
 	"fmt"
-	cmp "github.com/cornelk/hashmap"
+
+	"github.com/alphadose/haxmap"
 )
 
 func main() {
+	// initialize map with key type `int` and value type `string`
+	mep := haxmap.New[int, string]()
 
-	m := cmp.New[uint8, int]()
-	m.Set(1, 123)
-	value, ok := m.Get(1)
+	// set a value (overwrites existing value if present)
+	mep.Set(1, "one")
 
-	fmt.Printf("value is: %d,ok is %t\n", value, ok)
+	// get the value and print it
+	val, ok := mep.Get(1)
+	if ok {
+		println(val)
+	}
 
-	n := cmp.New[string, int]()
-	n.Set("amount", 456)
-	value, ok = n.Get("amount")
+	mep.Set(2, "two")
+	mep.Set(3, "three")
+	mep.Set(4, "four")
 
-	fmt.Printf("value is: %d,ok is %t\n", value, ok)
+	// ForEach loop to iterate over all key-value pairs and execute the given lambda
+	mep.ForEach(func(key int, value string) bool {
+		fmt.Printf("Key -> %d | Value -> %s\n", key, value)
+		return true // return `true` to continue iteration and `false` to break iteration
+	})
 
+	mep.Del(1) // delete a value
+	mep.Del(0) // delete is safe even if a key doesn't exists
+
+	// bulk deletion is supported too in the same API call
+	// has better performance than deleting keys one by one
+	mep.Del(2, 3, 4)
+
+	if mep.Len() == 0 {
+		println("cleanup complete")
+	}
 }
