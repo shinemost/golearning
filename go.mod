da402af642348ed2795ef694f1307cc7aeb03e0f
shinemost <supertain147@163.com>
2023-12-24 23:00:49 +0800

    使用三方包sourcearaph/conc可以扩展sync.WaitGroup,简化代码，更好的封装，panic时提供了堆栈信息，同时提供了panic的保护机制。同时还提供了其他的同步原语，可以详细了解，比如goroutine Pool，slice和map并发处理 https://github.com/sourcegraph/conc

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module golearning
 go 1.21
 
 require (
-	github.com/alphadose/haxmap v1.3.1
 	github.com/orcaman/concurrent-map v1.0.0
+	github.com/sourcegraph/conc v0.3.0
 )
-
-require golang.org/x/exp v0.0.0-20221031165847-c99f073a8326 // indirect
--- main.go
+++ main.go
@@ -3,40 +3,37 @@ package main
 import (
 	"fmt"
 
-	"github.com/alphadose/haxmap"
+	"github.com/sourcegraph/conc"
 )
 
 func main() {
-	// initialize map with key type `int` and value type `string`
-	mep := haxmap.New[int, string]()
-
-	// set a value (overwrites existing value if present)
-	mep.Set(1, "one")
-
-	// get the value and print it
-	val, ok := mep.Get(1)
-	if ok {
-		println(val)
+	var wg conc.WaitGroup
+	for i := 0; i < 10; i++ {
+		i := i
+		wg.Go(func() {
+			//m := 0
+			//n := i / m
+			//fmt.Println(n)
+			fmt.Println(i)
+		})
 	}
+	//wg.Wait()
+	wg.WaitAndRecover()
 
-	mep.Set(2, "two")
-	mep.Set(3, "three")
-	mep.Set(4, "four")
-
-	// ForEach loop to iterate over all key-value pairs and execute the given lambda
-	mep.ForEach(func(key int, value string) bool {
-		fmt.Printf("Key -> %d | Value -> %s\n", key, value)
-		return true // return `true` to continue iteration and `false` to break iteration
-	})
-
-	mep.Del(1) // delete a value
-	mep.Del(0) // delete is safe even if a key doesn't exists
-
-	// bulk deletion is supported too in the same API call
-	// has better performance than deleting keys one by one
-	mep.Del(2, 3, 4)
-
-	if mep.Len() == 0 {
-		println("cleanup complete")
-	}
 }
+
+/*func main() {
+	var wg sync.WaitGroup
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		i := i
+		go func() {
+			defer wg.Done()
+			m := 0
+			n := i / m
+			fmt.Println(n)
+		}()
+
+	}
+	wg.Wait()
+}*/
