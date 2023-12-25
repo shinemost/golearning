96d87ea6be97e30905d3faad357503bdec0a4d57
shinemost <supertain147@163.com>
2023-12-25 22:07:57 +0800

    使用sync.Pool池化技术缓存复用对象

--- main.go
+++ main.go
@@ -2,38 +2,46 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/sourcegraph/conc"
+	"net/http"
+	"sync"
+	"time"
 )
 
 func main() {
-	var wg conc.WaitGroup
-	for i := 0; i < 10; i++ {
-		i := i
-		wg.Go(func() {
-			//m := 0
-			//n := i / m
-			//fmt.Println(n)
-			fmt.Println(i)
-		})
+	var p sync.Pool
+
+	p.New = func() any {
+		return &http.Client{
+			Timeout: 5 * time.Second,
+		}
 	}
-	//wg.Wait()
-	wg.WaitAndRecover()
 
-}
-
-/*func main() {
 	var wg sync.WaitGroup
 	wg.Add(10)
-	for i := 0; i < 10; i++ {
-		i := i
-		go func() {
-			defer wg.Done()
-			m := 0
-			n := i / m
-			fmt.Println(n)
-		}()
+	go func() {
 
-	}
+		for i := 0; i < 10; i++ {
+			go func() {
+				defer wg.Done()
+
+				c := p.Get().(*http.Client)
+				defer p.Put(c)
+				resp, err := c.Get("https://www.baidu.com")
+				if err != nil {
+					fmt.Println("failed to get baidu.com:", err)
+					return
+				}
+
+				err = resp.Body.Close()
+				if err != nil {
+					fmt.Println("close response error:", err)
+					return
+				}
+				fmt.Println("get baidu.com")
+
+			}()
+		}
+	}()
 	wg.Wait()
-}*/
+
+}
