83ed13c6847e19ca27a43a7888bca6d78d8ef09c
shinemost <supertain147@163.com>
2024-01-01 16:53:28 +0800

    将同一个channel的发送和接收作为select语句的case字句。

--- main.go
+++ main.go
@@ -1,45 +1,15 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/oxtoacart/bpool"
-)
-
-var bufpool *bpool.BufferPool
+import "fmt"
 
 func main() {
+	var ch = make(chan int, 10)
 
-	//数据库连接池和http client连接池底层都是用了Sync.Pool技术，有时间可以看看源码学习一下
-	//sql.DB{}
-	//http.Transport{}
-
-	//三方包
-	//bytebufferpool 底层是sync.Pool https://github.com/valyala/bytebufferpool/
-	//oxtoacart/bpool 底层基于channel实现的 https://github.com/oxtoacart/bpool
-
-	bufpool = bpool.NewBufferPool(48)
-
-	fmt.Println(bufpool.NumPooled())
-
-	buf := bufpool.Get()
-
-	fmt.Println(bufpool.NumPooled())
-
-	bufpool.Put(buf)
-
-	fmt.Println(bufpool.NumPooled())
-}
-
-func someFunction() error {
-
-	// Get a buffer from the pool
-	buf := bufpool.Get()
-	//...
-	//...
-	//...
-	// Return the buffer to the pool
-	bufpool.Put(buf)
-
-	return nil
+	for i := 0; i < 10; i++ {
+		select {
+		case ch <- i:
+		case v := <-ch:
+			fmt.Println(v)
+		}
+	}
 }
