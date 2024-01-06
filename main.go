923f386dcc939b35a80a6c9d28a216a2b73c36ff
shinemost <supertain147@163.com>
2024-01-06 22:16:15 +0800

    使用channel实现信号量

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
-var ch = make(chan struct {
-})
-
-var s string
-
-func f() {
-	s = "hello world"
-	<-ch
-}
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	go f()
-	ch <- struct{}{}
-	print(s)
+	const concurrentAccess = 3
+
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, concurrentAccess)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+
+			sem <- struct{}{} // Acquire semaphore
+			defer func() {
+				<-sem // Release semaphore
+			}()
+
+			// Simulate some work
+			fmt.Printf("Goroutine %d: Start\n", id)
+			// Do some work...
+			fmt.Printf("Goroutine %d: End\n", id)
+		}(i)
+	}
+
+	wg.Wait()
 }
