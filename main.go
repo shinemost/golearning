84d5f05c9e6017b31f43d6f327e2c529ea613e20
shinemost <supertain147@163.com>
2024-01-06 22:44:10 +0800

    使用三方库marusama/semaphore作为信号量实现同样的逻辑，可以动态更改资源容量，setLimit() https://github.com/marusama/semaphore

--- main.go
+++ main.go
@@ -1,29 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
+	"time"
+
+	"github.com/marusama/semaphore/v2"
 )
 
 func main() {
-	const concurrentAccess = 3
-
+	sem := semaphore.New(3)
 	var wg sync.WaitGroup
-	sem := make(chan struct{}, concurrentAccess)
-
+	wg.Add(10)
 	for i := 0; i < 10; i++ {
-		wg.Add(1)
+
 		go func(id int) {
 			defer wg.Done()
 
-			sem <- struct{}{} // Acquire semaphore
-			defer func() {
-				<-sem // Release semaphore
-			}()
+			err := sem.Acquire(context.Background(), 1)
+			if err != nil {
+				return
+			} // Acquire semaphore
+
+			//count := sem.GetCount()
+			//println(count)
+			sem.SetLimit(4)
+			println(sem.GetCount())
+			defer sem.Release(1) // Release semaphore
 
 			// Simulate some work
 			fmt.Printf("Goroutine %d: Start\n", id)
 			// Do some work...
+			time.Sleep(3 * time.Second)
 			fmt.Printf("Goroutine %d: End\n", id)
 		}(i)
 	}
