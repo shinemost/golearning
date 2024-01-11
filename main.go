a256d9150d275ff2bdefd320fe3e3bb2bad73482
shinemost <supertain147@163.com>
2024-01-11 21:33:08 +0800

    使用额外的切片存放每一个协程的结果

--- main.go
+++ main.go
@@ -11,29 +11,32 @@ import (
 func main() {
 
 	var g errgroup.Group
+	var result = make([]error, 3)
 
 	g.Go(func() error {
 		time.Sleep(5 * time.Second)
 		fmt.Println("exec #1")
+		result[0] = nil
 		return nil
 	})
 
 	g.Go(func() error {
 		time.Sleep(10 * time.Second)
 		fmt.Println("exec #2")
-		return errors.New("failed to exec #2")
+		result[1] = errors.New("failed to exec #2")
+		return result[1]
 	})
 
 	g.Go(func() error {
 		time.Sleep(15 * time.Second)
 		fmt.Println("exec #3")
+		result[2] = nil
 		return nil
 	})
 
 	if err := g.Wait(); err == nil {
-		fmt.Println("Successfully exec all")
+		fmt.Printf("Successfully exec all.result: %v\n", result)
 	} else {
-		fmt.Println("failed:", err)
-
+		fmt.Printf("failed: %v\n", result)
 	}
 }
