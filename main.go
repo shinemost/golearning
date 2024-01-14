96a117b5a6b97bce7d1f5b8fc111b89ed7ee35d5
shinemost <supertain147@163.com>
2024-01-14 17:24:24 +0800

    使用三方包vardius/gollback race方法只要一个异步函数的执行结果没有错误，就立即返回，而不会返回所有子任务的信息，如果所有子任务都没有执行成功，则返回最后一个错误

--- main.go
+++ main.go
@@ -10,13 +10,15 @@ import (
 )
 
 func main() {
-	rs, errs := gollback.All(context.Background(), func(ctx context.Context) (interface{}, error) {
+	rs, errs := gollback.Race(context.Background(), func(ctx context.Context) (interface{}, error) {
 		time.Sleep(3 * time.Second)
-		return 1, nil
+		//return 1, nil
+		return nil, errors.New("failed1")
 	}, func(ctx context.Context) (interface{}, error) {
-		return nil, errors.New("failed")
+		return nil, errors.New("failed2")
 	}, func(ctx context.Context) (interface{}, error) {
-		return 3, nil
+		//return 3,nil;
+		return nil, errors.New("failed3")
 	})
 	fmt.Println(rs)
 	fmt.Println(errs)
