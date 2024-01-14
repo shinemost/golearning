0282fba9ed86aa284eb5da0661ec6bc068b0aff5
shinemost <supertain147@163.com>
2024-01-14 21:57:56 +0800

    使用三方包AaronJan/Hunch 的Waterfail方法，类似pipeline处理方法，子任务按顺序执行，前一个方法的result 作为后一个任务的入参，如果执行到一个任务报错，则立即返回错误，终止任务

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"time"
 
@@ -13,9 +12,14 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	res, err := hunch.Retry(ctx, 5, func(ctx context.Context) (interface{}, error) {
-		time.Sleep(6 * time.Second)
-		return nil, errors.New("failed")
+	res, err := hunch.Waterfall(ctx, func(ctx context.Context, i interface{}) (interface{}, error) {
+		n := 1
+		return n * 2, nil
+		//return nil, errors.New("failed")
+	}, func(ctx context.Context, i interface{}) (interface{}, error) {
+		fmt.Println("perivous fun result is : ", i)
+		//return nil, errors.New("failed")
+		return 4, nil
 	})
 
 	fmt.Println(res)
