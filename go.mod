a72028e4f71c6f9804f81d965cbf4bd6c8abcae4
shinemost <supertain147@163.com>
2024-01-14 14:12:09 +0800

    使用三方包vardius/gollback来实现组处理任务 https://github.com/vardius/gollback

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.21
 
 require github.com/orcaman/concurrent-map v1.0.0
 
-require github.com/go-pkgz/syncs v1.3.2
+require github.com/vardius/gollback v1.1.1
--- main.go
+++ main.go
@@ -2,27 +2,23 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"sync/atomic"
 	"time"
 
-	"github.com/go-pkgz/syncs"
+	"github.com/vardius/gollback"
 )
 
 func main() {
-	//默认是控制子任务的并发数量
-	swg := syncs.NewSizedGroup(10)
-	// 另一种处理方式，控制同时处理任务的协程数量
-	//swg := syncs.NewSizedGroup(10, syncs.Preemptive)
-	var c uint32
-
-	for i := 0; i < 1000; i++ {
-		swg.Go(func(ctx context.Context) {
-			time.Sleep(5 * time.Millisecond)
-			atomic.AddUint32(&c, 1)
-		})
-	}
-	swg.Wait()
-	fmt.Println(c)
+	rs, errs := gollback.All(context.Background(), func(ctx context.Context) (interface{}, error) {
+		time.Sleep(3 * time.Second)
+		return 1, nil
+	}, func(ctx context.Context) (interface{}, error) {
+		return nil, errors.New("failed")
+	}, func(ctx context.Context) (interface{}, error) {
+		return 3, nil
+	})
+	fmt.Println(rs)
+	fmt.Println(errs)
 
 }
