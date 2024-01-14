4eba5c81292ae633407a36247557e89735137de6
shinemost <supertain147@163.com>
2024-01-14 20:57:22 +0800

    使用三方包AaronJan/Hunch 的Take方法，指定num参数，只要有num个子任务正常执行没有错误，就会返回这几个子任务执行结果，一旦一个子任务发生错误，改方法就会返回错误信息。 https://github.com/AaronJan/Hunch

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"time"
 
@@ -13,13 +12,16 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	res, err := hunch.All(ctx, func(ctx context.Context) (interface{}, error) {
+	res, err := hunch.Take(ctx, 2, func(ctx context.Context) (interface{}, error) {
 		time.Sleep(3 * time.Second)
 		return 1, nil
 	}, func(ctx context.Context) (interface{}, error) {
 		//return 2, nil
-		return nil, errors.New("failed")
+		time.Sleep(4 * time.Second)
+		return 2, nil
+		//return nil, errors.New("failed")
 	}, func(ctx context.Context) (interface{}, error) {
+		time.Sleep(2 * time.Second)
 		return 3, nil
 	})
 
