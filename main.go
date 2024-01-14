cfa0af6eee669a87f04a74dc8d4297a0039bfb54
shinemost <supertain147@163.com>
2024-01-14 22:41:19 +0800

    使用三方包go-waitgroup扩展了sync.waitgroup，内部通过chan实现了类似线程池的效果，来处理子任务，也提供了对错误的处理方法NewErrorGroup内部使用了多个基础并发原语，包括chan,waitgroup,once,详情请查看源代码

--- main.go
+++ main.go
@@ -1,41 +1,32 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
-	"net/http"
+	"os"
 
 	"github.com/pieterclaerhout/go-waitgroup"
 )
 
 func main() {
 
-	urls := []string{
-		"https://www.easyjet.com/",
-		"https://www.skyscanner.de/",
-		"https://www.ryanair.com",
-		"https://wizzair.com/",
-		"https://www.swiss.com/",
+	ctx := context.Background()
+
+	wg, _ := waitgroup.NewErrorGroup(ctx, 5)
+
+	wg.Add(
+		func() error {
+			return nil
+		},
+		func() error {
+			return errors.New("an error occurred")
+		},
+	)
+
+	if err := wg.Wait(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
 	}
-
-	wg := waitgroup.NewWaitGroup(3)
-
-	for _, url := range urls {
-
-		urlToCheck := url
-		wg.Add(func() {
-			fmt.Printf("%s: checking\n", urlToCheck)
-			res, err := http.Get(urlToCheck)
-			if err != nil {
-				fmt.Println("Error: %v")
-			} else {
-				defer res.Body.Close()
-				fmt.Printf("%s: result: %v\n", urlToCheck, err)
-			}
-		})
-
-	}
-
-	wg.Wait()
-	fmt.Println("Finished")
 
 }
