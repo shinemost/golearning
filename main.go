afaf77bceabad0d8d281867c138ad684b15fbd0e
shinemost <supertain147@163.com>
2024-01-21 13:30:09 +0800

    使用三方包go.uber.org/ratelimit基于漏洞的限流库,withSlack这个方法需要特别注意，与书上所说是不同的，目前还未知道是什么原因 https://github.com/uber-go/ratelimit

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"time"
 
 	"go.uber.org/ratelimit"
 )
 
 func main() {
-	rl := ratelimit.New(100) // per second
+	rl := ratelimit.New(1, ratelimit.WithSlack(100)) // per second, no slack.
 
-	prev := time.Now()
-	for i := 0; i < 10; i++ {
-		now := rl.Take()
-		fmt.Println(i, now.Sub(prev))
-		prev = now
+	for i := 0; i < 200; i++ {
+		t := rl.Take()
+		log.Println(t)
+		log.Printf("got #%d", i)
+		if i == 3 {
+			time.Sleep(3 * time.Second)
+		}
 	}
 }
