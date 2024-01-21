8fdf8cb7e0869a568ed53f684b7522aa4cd80219
shinemost <supertain147@163.com>
2024-01-21 14:38:27 +0800

    使用分布式限流库go-redis/redis_rate底层使用Redis，通过lua脚本计算对应key的令牌情况，同于分布式应用来说，可使用同一个key https://github.com/go-redis/redis_rate

--- main.go
+++ main.go
@@ -2,25 +2,41 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
+	"sync"
+	"time"
 
 	"github.com/go-redis/redis_rate/v10"
 	"github.com/redis/go-redis/v9"
 )
 
 func main() {
-	ctx := context.Background()
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "admin",
-	})
-	_ = rdb.FlushDB(ctx).Err()
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-	limiter := redis_rate.NewLimiter(rdb)
-	res, err := limiter.Allow(ctx, "project:123", redis_rate.PerSecond(10))
-	if err != nil {
-		panic(err)
+	for i := 0; i < 2; i++ {
+		i := i
+		go func() {
+			defer wg.Done()
+			ctx := context.Background()
+			rdb := redis.NewClient(&redis.Options{
+				Addr:     "localhost:6379",
+				Password: "admin",
+			})
+			_ = rdb.FlushDB(ctx).Err()
+			limiter := redis_rate.NewLimiter(rdb)
+			for j := 0; j < 10; j++ {
+				res, err := limiter.Allow(ctx, "token:123", redis_rate.PerSecond(5))
+				if err != nil {
+					panic(err)
+				}
+				log.Println(i, "allowed", res.Allowed, "remaining", res.Remaining, "retry after", res.RetryAfter)
+				if res.Allowed == 0 {
+					time.Sleep(res.RetryAfter)
+				}
+			}
+		}()
 	}
-	fmt.Println("allowed", res.Allowed, "remaining", res.Remaining)
-	// Output: allowed 1 remaining 9
+	wg.Wait()
 }
