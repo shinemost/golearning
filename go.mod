a795e78791f1507533ed808e6307400496f60ffc
shinemost <supertain147@163.com>
2024-01-21 11:45:14 +0800

    使用三方包juju/ratelimit另一个高效的基于令牌桶的限流库,特色是支持一次生成多个令牌 https://github.com/juju/ratelimit

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.21
 
 require github.com/orcaman/concurrent-map v1.0.0
 
-require golang.org/x/time v0.5.0
+require (
+	github.com/juju/ratelimit v1.0.2
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- main.go
+++ main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
-	"context"
 	"log"
 	"time"
 
-	"golang.org/x/time/rate"
+	"github.com/juju/ratelimit"
 )
 
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-	var limiter = rate.NewLimiter(1, 3)
-	for i := 0; i < 3; i++ {
-		log.Printf("got #%d,err:%v", i, limiter.Wait(context.Background()))
+	var bucket = ratelimit.NewBucket(time.Second, 3)
+	for i := 0; i < 10; i++ {
+		bucket.Wait(1)
+		log.Printf("got #%d", i)
 	}
-
-	log.Println("set new limit at 10s")
-
-	//设置新速率为每3秒生成一个令牌，逻辑是先计算给定的时间t减去上一次生成令牌的时间，如果t在last之前，则不会增加
-	//新的令牌，等于说是毫无变化，维持原样，如果大于则会在这个时间差下按照原速率能产生多少令牌，如果多于令牌桶的
-	//容量也会设置成令牌桶的容量，然后返回新的令牌桶，桶是满的，而速率是新的速率
-	limiter.SetLimitAt(time.Now().Add(10*time.Second), rate.Every(3*time.Second))
-
-	for i := 4; i < 9; i++ {
-		log.Printf("got #%d,err:%v", i, limiter.Wait(context.Background()))
-	}
-
 }
