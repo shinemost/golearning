a205c80e21c8aa340c74082b48fc7096826c0c66
shinemost <supertain147@163.com>
2024-02-13 22:40:43 +0800

    周期性定时器Ticker的使用，要注意的是需要手动关闭ticker，否则会造成资源泄露，CPU缓慢升高直到满载，最好在创建ticker时采用defer ticker.Stop方法关闭

--- main.go
+++ main.go
@@ -6,50 +6,15 @@ import (
 )
 
 func main() {
-	AfterFuncDemo()
+	TickerDemo()
 }
 
-// WaitChannel 设置超时时间
-func WaitChannel(conn <-chan string) bool {
-	timer := time.NewTimer(time.Second)
+func TickerDemo() {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
 
-	select {
-	case <-conn:
-		timer.Stop()
-		return true
-	case <-timer.C:
-		println("waitChannel timeout!")
-		return false
+	for range ticker.C {
+		log.Println("Ticker tick.")
 	}
-}
-
-// DelayDoSomething 延迟执行逻辑
-func DelayDoSomething() {
-	timer := time.NewTimer(5 * time.Second)
-
-	select {
-	case <-timer.C:
-		println("delay 5 seconds to do something!")
-
-	}
-}
-
-// AfterDemo 简单接口After，等待对应的时间
-func AfterDemo() {
-	log.Println(time.Now())
-	<-time.After(time.Second)
-	log.Println(time.Now())
-}
-
-// AfterFuncDemo 延迟执行函数
-func AfterFuncDemo() {
-	log.Println(time.Now())
-	_ = time.AfterFunc(2*time.Second, func() {
-		log.Println("after func end:", time.Now())
-	})
-
-	//可以取消定时器
-	//timer.Stop()
-	time.Sleep(2 * time.Second)
 
 }
